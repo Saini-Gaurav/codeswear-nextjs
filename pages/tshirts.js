e8ac38c4039952ff7a294624b7a54382bc08ae87
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-01-04 22:20:21 +0530

    Displaying tshirts

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,144 +1,50 @@
 import Link from "next/link";
 import React from "react";
+import Product from "@/models/Product";
+import mongoose from "mongoose";
 
-const Tshirts = () => {
+const Tshirts = ({ products }) => {
   return (
     <div>
       <div className="row">
-        <div className="col-sm-3 pt-4 pb-4">
-          <Link href={"/product/wear-the-code"} className="text-decoration-none">
-            <div className="card">
-              <img
-                src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-                alt="Tshirts"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Tshirts</h5>
-                <p className="card-text">Wear the Code.</p>
-                <p className="card-text">₹ 499</p>
-                <p className="card-text">S, M, L, XL, XXL</p>
-              </div>
+        {products.map((item) => {
+          return (
+            <div key={item._id} className="col-sm-3 pt-4 pb-4">
+              <Link
+                passHref={true}
+                href={`/product/${item.slug}`}
+                className="text-decoration-none"
+              >
+                <div className="card" >
+                  <img
+                  style={{height: "40vh", objectFit: "cover"}}
+                    src={item.img}
+                    alt="Tshirts"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">Tshirts</h5>
+                    <p className="card-text">{item.title}</p>
+                    <p className="card-text">{item.price}</p>
+                    <p className="card-text">S, M, L, XL, XXL</p>
+                  </div>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>
-            </div>
-          </div>
-          </Link>
-        </div>
-        <div className="col-sm-3 pt-4 pb-4">
-        <Link href={"/product/wear-the-code"} className="text-decoration-none">
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81GFLtGfAnL._SX569_.jpg"
-              alt="Tshirts"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Tshirts</h5>
-              <p className="card-text">Wear the Code.</p>
-              <p className="card-text">₹ 499</p>
-              <p className="card-text">S, M, L, XL, XXL</p>      
-            </div>
-          </div>
-          </Link>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find({category: 'tshirt'});
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) },
+  };
+}
 
 export default Tshirts;
